1aab748f0cb072bf5e14e09deef52be88b986c2a
shahzaib-shams <shahzaib@aptechgdn.net>
2024-07-24 02:53:09 +0500

    files

---
+++ routes/authors.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const author = require('../models/author')
+const router = express.Router()
+const Author = require('../models/author')
+
+//All Author Route
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', { authors: authors, searchOptions: req.query })
+    } catch {
+        res.render('/')
+    }
+
+})
+
+//New Author Route
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() })
+})
+
+//CREATE AUTHOR ROUTE (POST for CREATE in CRUD)
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors')
+    } catch {
+        res.render('author/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        })
+    }
+})
+
+module.exports = router
